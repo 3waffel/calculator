423005392b4238927cb57216d793af2024b133c2
ワフ <45911671+3waffel@users.noreply.github.com>
2022-11-27 17:57:22 +0000

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,31 @@
-use dioxus::events::KeyCode;
-use dioxus::prelude::*;
+//! simple calculator
 
 #[macro_use]
 extern crate lalrpop_util;
-
 lalrpop_mod!(pub grammar);
+
 pub mod ast;
 
 fn main() {
-    dioxus_tui::launch(app);
+    raw_input();
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Panel {
-    Record,
-    Input,
-}
-
-impl Panel {
-    pub fn flip(self) -> Self {
-        match self {
-            Panel::Record => Panel::Input,
-            Panel::Input => Panel::Record,
-        }
-    }
-}
-
-fn app(cx: Scope) -> Element {
-    let mut records = vec!["1", "2", "3"];
-    let focusing = use_state(&cx, || Panel::Input);
-
-    cx.render(rsx! {
-        div {
-            display: "flex",
-            flex_direction: "row",
-            width: "100%",
-            height: "100%",
-            onkeydown: move |e| {
-                match e.data.key_code {
-                    KeyCode::LeftArrow => {
-                        if focusing.get() == &Panel::Input {
-                            focusing.set(Panel::Record);
-                        }
-                    }
-                    KeyCode::RightArrow => {
-                        if focusing.get() == &Panel::Record {
-                            focusing.set(Panel::Input);
-                        }
-                    }
-                    _ => {
-
-                    }
-                }
-            },
-            div {
-                div {
-                    border_style: "solid",
-                    border_color: format_args!("{}",
-                        if focusing.get() == &Panel::Record {"cyan"} else {"default"}
-                    ),
-                    justify_content: "center",
-                    align_items: "center",
-                    "Record Area",
-                    ul {
-                        {records.iter().map(|f| rsx!(li {"a"}))}
-                    }
-                },
-                div {
-                    border_style: "solid",
-                    border_color: format_args!("{}",
-                        if focusing.get() == &Panel::Input {"cyan"} else {"default"}
-                    ),
-                    justify_content: "center",
-                    align_items: "center",
-                    "Input Area",
-                    button {
-                        border_style: "solid",
-                        onclick: move |e| {},
-                        justify_content: "center",
-                        align_items: "center",
-                        display: "flex",
-                        flex_direction: "column",
-                        "Enter"
-                    }
-                }
+fn raw_input() {
+    let mut input = String::new();
+    match std::io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            println!("{n} bytes read: {input}");
+            let parse = grammar::ExprParser::new().parse(&input);
+            if parse.is_ok() {
+                println!("{:?}", parse.unwrap());
+            } else {
+                println!("{input} is an invalid expression.");
             }
         }
-    })
+        Err(error) => {
+            println!("error: {error}")
+        }
+    }
 }
 
 #[test]
